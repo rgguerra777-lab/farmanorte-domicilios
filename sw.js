a4f87546b8cff2a6fdd6e82b4dfdba5091fa2ee8
rgguerra777-lab <rgguerra777-lab@users.noreply.github.com>
2026-06-11 17:35:24 -0500

    Agregar login de equipo, estadísticas con gráficas y nueva identidad visual

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'farmanorte-domicilios-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE_NAME = 'farmanorte-domicilios-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './logo.jpg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
